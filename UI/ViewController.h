efc979b4e59ce86792e91905d6022d45b1d3a101
wangyan66 <819156618@qq.com>
2019-10-28 12:42:40 +0800

    Initial Commit

---
+++ UI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UI
+//
+//  Created by 王焱 on 2019/10/28.
+//  Copyright © 2019年 王焱. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UI/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UI
+//
+//  Created by 王焱 on 2019/10/28.
+//  Copyright © 2019年 王焱. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
